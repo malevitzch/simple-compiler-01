f0d5e3af189e67366b5bd47e701f731f088df27b
malevitzch <malevitzch@gmail.com>
2024-06-28 12:08:50 +0200

    Compiler actually works now, now comes the worst part - actually interpreting text files

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <fstream>
 #include "../include/compiler.hpp"
 
 int main()
 {
-  std::cout<<asm_getters::create_variable(15);
-  std::cout<<asm_getters::create_variable(18);
-  std::cout<<asm_getters::create_variable(12);
-  std::cout<<asm_getters::modify_variable(7, 1, 2);
+  int ptr = 0;
+  std::ofstream output("debug_asm.S");
+  std::map<string, int> var_table;
+  output << asm_getters::base_template();
+  operations::declare_variable(output, var_table, "a", 5, ptr);
+  operations::print_variable(output, var_table, "a", ptr);
+  output << asm_getters::end_program();
 }
