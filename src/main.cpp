d89ce24c0a77158464f62f2da3808179501e2305
malevitzch <malevitzch@gmail.com>
2024-06-28 13:23:29 +0200

    temporary main changes

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,10 @@ int main()
   std::ofstream output("debug_asm.S");
   std::map<string, int> var_table;
   output << asm_getters::base_template();
+  operations::declare_variable(output, var_table, "b", 1, ptr);
   operations::declare_variable(output, var_table, "a", 5, ptr);
+  operations::declare_variable(output, var_table, "c", 7, ptr);
+  operations::assign_to_variable(output, var_table, "a", 17, ptr);
   operations::print_variable(output, var_table, "a", ptr);
   output << asm_getters::end_program();
 }
