112a28d8658d99fa5a7c4e6a23679a7777ae9af9
malevitzch <malevitzch@gmail.com>
2024-06-28 16:47:17 +0200

    Added definitions for Compiler class functions that used to be free functions

--- include/compiler.hpp
+++ include/compiler.hpp
@@ -3,9 +3,6 @@
 #include <map>
 using std::string;
 using std::ostream;
-bool is_variable_registered(const std::map<string, int>& variable_table, const string name);
-//Registers variable in the specified variable table with set name and value, increments the stack ptr by 1. If a variable of the specified value is 
-bool register_variable(std::map<string, int>& variable_table, const string name, const int value, int& stack_ptr);
 
 class Compiler
 {
@@ -13,25 +10,27 @@ private:
   ostream& target;
   int stack_ptr = 0;
   std::map<string, int> variable_table;
+  bool is_variable_registered(string name) const;
+  bool register_variable(string name, int value);
 public:
   Compiler() = delete;
   Compiler(ostream& target);
   //TODO: implement the operations functions
-  bool declare_variable(const string name, const int value);
-  bool assign_to_variable(const string name, const int value);
-  bool print_variable(const string name) const;
+  bool declare_variable(string name, int value);
+  bool assign_to_variable(string name, int value);
+  bool print_variable(string name) const;
 };
 
 namespace asm_getters
 {
-  string create_variable(const int value);
-  string modify_variable(const int value, const int target_ptr, const int stack_ptr);
-  string print_variable(const int target_ptr, const int stack_ptr);
+  string create_variable(int value);
+  string modify_variable(int value, int target_ptr, int stack_ptr);
+  string print_variable(int target_ptr, int stack_ptr);
   string base_template();
   string end_program();
 }
 
-//namespace operations and its member functions are deprecated
+//TODO: remove when no longer needed
 namespace operations
 {
   bool declare_variable(ostream& target, std::map<string, int>& variable_table, const string name, const int value, int& stack_ptr);
