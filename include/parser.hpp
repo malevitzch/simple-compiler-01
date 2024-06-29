9a89e6de52e5d42b566fcd5d1a6dd59819244c4d
malevitzch <malevitzch@gmail.com>
2024-06-29 15:54:44 +0200

    Added classes for Operations (early work towards implementing a parser)

--- include/parser.hpp
+++ include/parser.hpp
@@ -1,13 +1,44 @@
-#include <iostream>
+#pragma once
 #include "compiler.hpp"
 
 //all operations inherit from this abstract class so
 class Operation
 {
 private:
+protected:
+  Operation();
 public:
-  Operation() = delete;
   virtual void process(Compiler& compiler) = 0;
 };
 
+class DeclarationOperation : public Operation
+{
+private:
+  DeclarationOperation() = delete;
+  string variable_name;
+  int value;
+public:
+  DeclarationOperation(string variable_name, int value);
+  void process(Compiler& compiler);
+};
 
+class AssignmentOperation : public Operation
+{
+private:
+  AssignmentOperation() = delete;
+  string variable_name;
+  int value;
+public:
+  AssignmentOperation(string variable_name, int value);
+  void process(Compiler& compiler);
+};
+
+class PrintOperation : public Operation
+{
+private:
+  PrintOperation() = delete;
+  string variable_name;
+public:
+  PrintOperation(string variable_name);
+  void process(Compiler& compiler);
+};
