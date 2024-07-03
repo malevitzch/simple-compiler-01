084e55b65e3c95b97f34e6bfa8ba6160b2aef77d
malevitzch <malevitzch@gmail.com>
2024-07-03 10:24:42 +0200

    Linter now properly detects brackets and operators, not as if they matter for now but it's still cool that it does

--- include/linter.hpp
+++ include/linter.hpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 #include <vector>
 using std::string;
 
+enum class CharType
+{
+  None,
+  Regular,
+  Operator,
+  Singleton
+};
+
+bool is_singleton(char ch);
 std::vector<std::vector<string>> tokenize_file(string filename); 
--- src/linter.cpp
+++ src/linter.cpp
@@ -1,6 +1,23 @@
 #include "../include/linter.hpp"
-
+#include <set>
 //This assumes the file exists. Todo: add error handling. I'm thinking variant or optional return type?
+
+bool is_singleton(char ch)
+{
+  std::set<char> singletons = 
+  {
+    '(', ')', '{', '}', '[', ']', '?', '!', 
+  };
+  return singletons.find(ch) != singletons.end();
+}
+bool is_operator(char ch)
+{
+  std::set<char> operators = 
+  {
+    '+', '-', '*', '/', '>', '<', '=', '#'
+  };
+  return operators.find(ch) != operators.end();
+}
 std::vector<std::vector<string>> tokenize_file(string filename)
 {
   std::vector<std::vector<string>> tokenized_file;
@@ -8,6 +25,7 @@ std::vector<std::vector<string>> tokenize_file(string filename)
   
   file_stream >> std::noskipws;
   bool finished = false;
+  CharType cur_type = CharType::None;
   char cur_char;
   string cur_token = "";
   std::vector<string> cur_statement = {};
@@ -34,6 +52,7 @@ std::vector<std::vector<string>> tokenize_file(string filename)
         tokenized_file.push_back(cur_statement);
         cur_statement = {};
       }
+      cur_type = CharType::None;
     }
     else if(isspace(cur_char))
     {
@@ -42,11 +61,48 @@ std::vector<std::vector<string>> tokenize_file(string filename)
         cur_statement.push_back(cur_token);
         cur_token = "";
       }
-
+      cur_type = CharType::None;
     }
     else
     {
+      if(is_singleton(cur_char))
+      {
+        if(!cur_token.empty())
+        {
+          cur_statement.push_back(cur_token);
+          cur_token = "";
+        }
+        string temp = "";
+        temp += cur_char;
+        cur_statement.push_back(temp);
+        cur_type = CharType::None;
+      }
+      else if(is_operator(cur_char))
+      {
+        if(cur_type != CharType::Operator && cur_type != CharType::None)
+        {
+          if(!cur_token.empty())
+          {
+            cur_statement.push_back(cur_token);
+            cur_token = "";
+          }
+        }
       cur_token += cur_char;
+      cur_type = CharType::Operator;
+      }
+      else
+      {
+        //TODO: Add check for character being 'Unknown' 
+        if(cur_type != CharType::Regular && cur_type != CharType::None)
+        {
+          if(!cur_token.empty())
+          {
+            cur_statement.push_back(cur_token);
+            cur_token = "";
+          } 
+        }
+        cur_token += cur_char;
+      }
     }
   }
   return tokenized_file;
