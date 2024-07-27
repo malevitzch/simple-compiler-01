6c6c74be1a931688f43c1f9fc4fff8349374532a
malevitzch <malevitzch@gmail.com>
2024-07-27 12:56:11 +0200

    Beginning work on the shunting yard algorithm for expression parsing

---
+++ include/shunting_yard.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+#include <vector>
+using std::string;
+bool is_operator(string token);
+void prefix_unary(std::vector<string>& expression);
---
+++ src/shunting_yard.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+
+#include "operators.hpp"
+#include "shunting_yard.hpp"
+
+bool is_operator(string token)
+{
+  std::vector<string> operators = get_operator_symbols();
+  return std::find(operators.begin(), operators.end(), token) != operators.end();
+}
+
+void prefix_unary(std::vector<string>& expression)
+{
+  bool last_null_or_operator = true;
+  for(string& token : expression)
+  {
+    //we ignore parentheses
+    if(token == "(" || token == ")") continue;
+
+    if(is_operator(token))
+    {
+      //an unary operator is either the first token of an expression or directly follows another operator (excluding brackets of course)
+      if(last_null_or_operator) token = "u" + token;
+      last_null_or_operator = true;
+    }
+    else
+    {
+      last_null_or_operator = false;
+    }
+  }
+}
