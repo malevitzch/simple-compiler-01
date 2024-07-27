98d19be8a1890bfe11cad4249cade4a82f7d29fe
malevitzch <malevitzch@gmail.com>
2024-07-27 11:51:39 +0200

    Added associativity to operator class

--- include/operators.hpp
+++ include/operators.hpp
@@ -6,11 +6,17 @@ enum class OperatorType
   Unary,
   Binary
 };
+enum class Associativity
+{
+  Left, 
+  Right
+};
 struct Operator
 {
   string name; //de facto the default key of operator as stuff like "-" can be both unary and binary
   string symbol;
   OperatorType type;
   int precedence;
-  Operator(string name, string symbol, OperatorType type, int precedence);
+  Associativity associativity;
+  Operator(string name, string symbol, OperatorType type, int precedence, Associativity associativity);
 };
--- src/operators.cpp
+++ src/operators.cpp
@@ -1,3 +1,3 @@
 #include "operators.hpp"
 
-Operator::Operator(string name, string symbol, OperatorType type, int precedence) : name(name), symbol(symbol), type(type), precedence(precedence) {}
+Operator::Operator(string name, string symbol, OperatorType type, int precedence, Associativity associativity) : name(name), symbol(symbol), type(type), precedence(precedence), associativity(associativity) {}
