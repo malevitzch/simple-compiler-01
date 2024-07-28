b8955162f33cd0e71749134b433deb135c464d16
malevitzch <malevitzch@gmail.com>
2024-07-28 11:22:19 +0200

    Implemented parentheses handling in the shunting yard algorithm

--- src/shunting_yard.cpp
+++ src/shunting_yard.cpp
@@ -1,4 +1,6 @@
 #include <algorithm>
+#include <stack>
+#include <stdexcept>
 
 #include "operators.hpp"
 #include "shunting_yard.hpp"
@@ -16,7 +18,7 @@ void prefix_unary(std::vector<string>& expression)
   {
     //we ignore parentheses
     if(token == "(" || token == ")") continue;
-
+    //TODO: syntax is not being checked here, double binary operators might get prefixed which is not desired
     if(is_operator(token))
     {
       //an unary operator is either the first token of an expression or directly follows another operator (excluding brackets of course)
@@ -33,8 +35,38 @@ void prefix_unary(std::vector<string>& expression)
 std::vector<string> infix_to_postfix(std::vector<string> expression)
 {
   prefix_unary(expression);
-
-  std::vector<string> posfix_expression;
   
-  return posfix_expression;
+  std::vector<string> postfix_expression;
+  std::stack<string> operator_stack;
+
+  //operator map contains operators already prefixed in case of unary oness
+  std::map<string, Operator> operator_map = get_operator_map();
+
+  for(string token : expression)
+  {
+    if(token == ")")
+    {
+      while(1)
+      {
+        if(operator_stack.empty())
+        {
+          throw std::runtime_error("Mismatched parentheses");
+        }
+        string cur = operator_stack.top();
+        operator_stack.pop();
+        if(cur == "(") break;
+        else postfix_expression.push_back(cur);
+      }
+    }
+    else if(operator_map.find(token) != operator_map.end())
+    {
+
+    }
+    else
+    {
+      postfix_expression.push_back(token);
+    }
+  }
+
+  return postfix_expression;
 }
